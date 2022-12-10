1a8882acbffc48df04255f695502ff537531668f
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-12-10 16:26:11 +0200

    fixed imports

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './auth/auth-slice';
 import { contactsReducer } from './contacts/slice';
 import storage from 'redux-persist/lib/storage';
